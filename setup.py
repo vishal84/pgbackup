1ee43cfd95c531be03fcc357640aebcc492c23ac
Vishal Patel <vishal3@gmail.com>
2020-04-06 17:57:50 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgpackup',
+    version='0.1.0',
+    author='Keith Thompson',
+    author_email='keith@linuxacademy.com',
+    description='A utility for backing up PostgreSQL databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/vishal84/pgbackup',
+    packages=find_packages('src')
+)
